6f340191e23eb01da39dee96aed05498e71c4f28
root <root@cdn.oss.letv.com>
2015-01-14 14:47:10 +0800

    Add the chgrp command

--- centos64/loop.sh
+++ centos64/loop.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 if [ ! -f /dev/loop2 ]; then
 mknod -m 660 /dev/loop2 b 7 2
+chgrp disk /dev/loop2
 fi
 if [ ! -f /dev/loopr3 ]; then
 mknod -m 660 /dev/loop3 b 7 3
+chgrp disk /dev/loop3
 fi
 if [ ! -f /dev/loop4 ]; then
 mknod -m 660 /dev/loop4 b 7 4
+chgrp disk /dev/loop4
 fi
 if [ ! -f /dev/loop5 ]; then 
 mknod -m 660 /dev/loop5 b 7 5
+chgrp disk /dev/loop5
 fi
-
